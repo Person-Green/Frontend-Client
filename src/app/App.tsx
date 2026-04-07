9ae70ddad8944c12499be6e0167aa6ae94c2af7f
soeun823 <bsoeun866@gmail.com>
2026-04-07 09:06:56 +0900

    feat (#9): 온보딩 화면 넘어가기

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,26 @@
-import { Route, Routes } from "react-router-dom";
-import Banner from "../widgets/banner";
-import Header from "../widgets/header";
-import Layout from "../widgets/layout";
-import BottomNav from "../widgets/bottomNav";
+// import { Route, Routes } from 'react-router-dom';
+// import Banner from '../widgets/banner';
+// import Header from '../widgets/header';
+// import Layout from '../widgets/layout';
+// import BottomNav from '../widgets/bottomNav';
+import Onboarding from '../pages/onboarding.tsx';
 
 function App() {
-
-  return (
-    <>
-      <Header/>
-      <Banner/>
-      <Routes>
-        <Route element={<Layout/>}>
-          {/* Pages */}
-        </Route>
-      </Routes>
-      <BottomNav />
-    </>
-  );
+  if (1) {
+    return <Onboarding />;
+  }
+  // return (
+  //   <>
+  //     <Header />
+  //     <Banner />
+  //     <Routes>
+  //       <Route element={<Layout />}>
+  //         {/* Pages */}
+  //       </Route>
+  //     </Routes>
+  //     <BottomNav />
+  //   </>
+  // );
 }
 
-export default App
+export default App;
---
+++ src/pages/onboarding.tsx
@@ -0,0 +1,88 @@
+import { useState } from 'react';
+import Plant1 from '../assets/plants/plant1.svg';
+import Plant2 from '../assets/plants/plant2.svg';
+import Plant3 from '../assets/plants/plant3.svg';
+
+const OnboardingPage = () => {
+  const itemList = [
+    {
+      id: 1,
+      img: Plant1,
+      title:
+        '오늘 내 공간에 ${<span class="text-color-primary">초록</span>}을 들여볼까요?',
+      description:
+        '몇가지 질문에 답해주시면,\n사용자님 공간에 알 맞는 식물을 추천해드립니다!',
+      button: '다음',
+      subTag: '건너뛰기',
+    },
+    {
+      id: 2,
+      img: Plant2,
+      title:
+        '${<span class="text-color-primary">간단</span>}하고, ${<span class="text-color-primary">쉬운</span>} 질문으로 준비했어요.',
+      description:
+        '어려운 식물 지식이 없어도,\n키워드에 맞는 환경을 알아서 분석해드려요.',
+      button: '다음',
+      subTag: '건너뛰기',
+    },
+    {
+      id: 3,
+      img: Plant3,
+      title:
+        '친구들과 식물을 ${<span class="text-color-primary">공유</span>}해보세요!',
+      description:
+        '좋아하는 식물들을 저장하고,\nSNS를 통해서 친구들에게 공유해보세요!',
+      button: '초록이 만나러가기',
+      subTag: '처음으로 돌아가기',
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentItem = itemList[currentIndex];
+  const isLastPage = currentIndex === itemList.length - 1;
+
+  const handleNext = () => {
+    if (isLastPage) {
+      return;
+    }
+
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const handleSubAction = () => {
+    if (isLastPage) {
+      setCurrentIndex(0);
+      return;
+    }
+
+    setCurrentIndex(itemList.length - 1);
+  };
+
+  return (
+    <main className={'h-fit'}>
+      <div key={currentItem.id}>
+        <div>
+          <img src={currentItem.img} alt="식물사진" />
+          <div>
+            <div>{currentItem.title}</div>
+            <div className="whitespace-pre-line">{currentItem.description}</div>
+          </div>
+          <div>
+            {currentItem.id}
+            <span>/{itemList.length}</span>
+          </div>
+        </div>
+        <div>
+          {/*  버튼 컴포넌트*/}
+          <button type="button" onClick={handleNext}>
+            {currentItem.button}
+          </button>
+          <button type="button" onClick={handleSubAction}>
+            {currentItem.subTag}
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default OnboardingPage;
