fcbc19da5fc9a4d013d96ad1bd985bb36fdda388
최정혁 <choejeonghyeog@choejeonghyeogs-MacBook-Air.local>
2026-04-07 11:36:41 +0900

    refactor(#14): Header, Banner, BottomNav 위치 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import Banner from "../widgets/banner";
-import Header from "../widgets/header";
 import Layout from "../widgets/layout";
-import BottomNav from "../widgets/bottomNav";
 import NotFound from "../pages/notfound";
 
 function App() {
 
   return (
     <>
-      <Header/>
-      <Banner/>
       <Routes>
         <Route element={<Layout/>}>
           {/* Pages */}
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <BottomNav />
     </>
   );
 }
--- src/widgets/layout.tsx
+++ src/widgets/layout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Header from "./header";
+import Banner from "./banner";
+import BottomNav from "./bottomNav";
 
 const Layout = () => {
   return(
 		<section>
+      <Header />
+      <Banner />
       <Outlet />
+      <BottomNav />
     </section>
   );
 }
