088b5e374d0638c3ab581d916ca21f33eee7c37e
soeun823 <bsoeun866@gmail.com>
2026-05-16 23:36:06 +0900

    feat: Google OAuth 인증 플로우 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from '../widgets/layout';
 import NotFound from '../pages/AuthFlow/notfound.tsx';
 import AuthFlow from '../pages/AuthFlow';
+import AuthCallback from '../pages/AuthFlow/callback.tsx';
+import EnterName from '../pages/AuthFlow/enterName.tsx';
 import Home from '../pages/Home';
 import Matching from '../pages/Matching';
 import MatchingSurvey from '../pages/Matching/Survey';
@@ -18,6 +20,8 @@ function App() {
         <Route path="/auth" element={<AuthFlow />} />
         <Route path="/matching/survey" element={<MatchingSurvey />} />
         <Route path="/matching/result" element={<MatchingResult />} />
+        <Route path="/auth/google/callback" element={<AuthCallback />} />
+        <Route path="/auth/enter-name" element={<EnterName />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
