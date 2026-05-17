4d851ae35dd8df6abfa813e63feed7a299cfc97b
최정혁 <choejeonghyeog@choejeonghyeogs-MacBook-Air.local>
2026-05-17 23:28:24 +0900

    refacctor(#31): button 컴포넌트 변경에 따른 modal 컴포넌트 변경과 Zustand 사용에 의한 shared 파일 구조 변경

--- src/pages/AuthFlow/login.tsx
+++ src/pages/AuthFlow/login.tsx
@@ -1,4 +1,4 @@
-import Button from '../../shared/button.tsx';
+import Button from '../../shared/ui/button.tsx';
 import AllPlant from '../../assets/plants/allPlant.svg';
 import Google from '../../assets/icon/google.svg';
 
