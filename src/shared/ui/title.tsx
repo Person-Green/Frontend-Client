17aea536706bacd27cf51726eb832bcb59caeacd
최정혁 <choejeonghyeog@choejeonghyeogs-MacBook-Air.local>
2026-05-18 00:05:15 +0900

    fix(#31): 홈페이지 퍼블리싱 오류 수정

--- src/shared/ui/title.tsx
+++ src/shared/ui/title.tsx
@@ -1,14 +1,13 @@
 interface TitleProps {
   icon: string | undefined;
   title: string;
-  textSize: string;
 }
 
 const Title = ({ icon, title, textSize }: TitleProps) => {
   return (
     <div className="flex items-center gap-6">
       <span className="icon-m text-text-highlight">{icon}</span>
-      <span className={textSize}>{title}</span>
+      <span className="title-m text-text-10">{title}</span>
     </div>
   );
 };
