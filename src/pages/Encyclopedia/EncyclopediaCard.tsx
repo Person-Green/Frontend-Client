e7684c8c3a36ac917549154858cb29d2be90b191
soeun823 <bsoeun866@gmail.com>
2026-05-19 19:36:54 +0900

    feat(#44): 식물도감페이지의 식물 상세 및 목록 API에 이미지 URL 추가 및 이미지 처리 로직 수정

--- src/pages/Encyclopedia/EncyclopediaCard.tsx
+++ src/pages/Encyclopedia/EncyclopediaCard.tsx
@@ -1,5 +1,5 @@
 import type { PlantCatalogItemResponse } from '../../entities';
-import Img from '../../assets/banner/banner1.svg';
+import NonePlantImage from '../../assets/plants/none.svg';
 
 interface EncyclopediaCardProps {
   plant: PlantCatalogItemResponse;
@@ -18,16 +18,19 @@ const formatFavoriteCount = (count: number): string => {
 };
 
 const EncyclopediaCard = ({ plant, onClick }: EncyclopediaCardProps) => {
+  const imageSrc =
+    plant.imageUrl && plant.imageUrl.trim() !== ''
+      ? plant.imageUrl
+      : NonePlantImage;
   return (
     <li
       onClick={onClick}
       className="flex flex-col gap-12 cursor-pointer min-w-0"
     >
-      <div className="relative w-full aspect-square rounded-8 bg-[#EEF2E6] overflow-hidden flex items-end justify-center">
-        <div className="w-[80%] h-[80%] flex items-center justify-center">
-          <img src={Img} alt={plant.plantKoreanName} />
-        </div>
-      </div>
+      <div
+        className="relative w-full aspect-square rounded-8 bg-[#EEF2E6] overflow-hidden bg-center bg-no-repeat bg-cover"
+        style={{ backgroundImage: `url(${imageSrc})` }}
+      />
 
       <div className="flex flex-col gap-6">
         <p className="body-m">
